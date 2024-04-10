9e498d2ae2d328bb3ef48b8b3baf52f24b41c3ce
lywang1989 <lywang1016@hotmail.com>
2024-04-10 14:59:59 +0800

    more informative

--- step0_pipeline_direct_usage.py
+++ step0_pipeline_direct_usage.py
@@ -3,11 +3,23 @@ warnings.filterwarnings('ignore')
 
 from transformers import pipeline
 
-En_Zh = pipeline("translation", model="Helsinki-NLP/opus-mt-en-zh")
-Zh_En = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
+# English to Chinese
+en_zh_translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-zh")
 
-res = En_Zh("How old are you?", src_lang='en', tgt_lang='zh') 
+res = en_zh_translator("How old are you?", src_lang='en', tgt_lang='zh') 
+print(res)
 print(res[0]['translation_text'])
 
-res = Zh_En("哎呦，你干嘛？", src_lang='zh', tgt_lang='en') 
+res = en_zh_translator(["Today is Friday.", "what\'s the most important thing?"], src_lang='en', tgt_lang='zh') 
+print(res)
+
+# Chinese to English
+zh_en_model = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
+
+res = zh_en_model("哎呦，你干嘛？", src_lang='zh', tgt_lang='en') 
+print(res[0]['translation_text'])
+
+# Test use default model
+default_model = pipeline("translation_en_to_fr")
+res = default_model("How old are you?") 
 print(res[0]['translation_text'])
