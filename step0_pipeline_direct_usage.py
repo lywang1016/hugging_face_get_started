a651e3102185e19ac68ffd60c670aca95df82468
lywang1989 <lywang1016@hotmail.com>
2024-04-10 15:03:21 +0800

    more informative

--- step0_pipeline_direct_usage.py
+++ step0_pipeline_direct_usage.py
@@ -14,12 +14,12 @@ res = en_zh_translator(["Today is Friday.", "what\'s the most important thing?"]
 print(res)
 
 # Chinese to English
-zh_en_model = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
+zh_en_translator = pipeline("translation", model="Helsinki-NLP/opus-mt-zh-en")
 
-res = zh_en_model("哎呦，你干嘛？", src_lang='zh', tgt_lang='en') 
+res = zh_en_translator("哎呦，你干嘛？", src_lang='zh', tgt_lang='en') 
 print(res[0]['translation_text'])
 
 # Test use default model
-default_model = pipeline("translation_en_to_fr")
-res = default_model("How old are you?") 
+default_translator = pipeline("translation_en_to_fr")
+res = default_translator("How old are you?") 
 print(res[0]['translation_text'])
